95265023a4a24ccfbd00f8b0ebedb83e2cf6d425
sleeplesseditor <josh@bryson.me.uk>
2018-03-14 18:05:23 +0000

    LinkList component expanded

---
+++ client/components/LinkList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+import { Links } from '../../imports/collections/links';
+
+class LinkList extends Component {
+    render() {
+        return (
+            <div>
+                List of Links to Click
+            </div>
+        );
+    }
+}
+
+export default createContainer(() => {
+    Meteor.subscribe('links');
+
+    return { links: Links.find({}).fetch() };
+}, LinkList);
--- server/main.js
+++ server/main.js
@@ -2,5 +2,8 @@ import { Meteor } from 'meteor/meteor';
 import { Links } from '../imports/collections/links';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  //Publication
+  Meteor.publish('links', function() {
+    return Links.find({});
+  });
 });
